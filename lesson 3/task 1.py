b9234c9214c681d3c9f14e277e8ecc0f3df223a5
V <mukhitov.aaa@list.ru>
2023-04-20 18:54:54 +0500

    #1 Выполнил задачу 1

--- lesson 3/task 1.py
+++ lesson 3/task 1.py
@@ -2,5 +2,5 @@ day = 20
 month = 'Апреля'
 temperature = 9
 print(f'Сегодня {day} {month}. На улице {temperature} градусов.')
-if temperature > 0:
+if temperature < 0:
     print('Холодно, лучше остаться дома')
